c21e661ae9863af11c835da55804df88865fd4ea
Leviski Njuguna <leviskinjuguna@gmail.com>
2026-01-28 19:53:01 +0300

    Updated the serializers to match current fields

--- backend/sections/serializers.py
+++ backend/sections/serializers.py
@@ -3,7 +3,9 @@ from .models import Section
 
 
 class SectionSerializer(serializers.ModelSerializer):
+    section_id = serializers.ReadOnlyField()
+    
     class Meta:
         model = Section
-        fields = ['id', 'name', 'code', 'description', 'created_at', 'updated_at']
+        fields = ['id', 'section_id', 'district', 'name', 'created_at', 'updated_at']
         read_only_fields = ['id', 'created_at', 'updated_at']
