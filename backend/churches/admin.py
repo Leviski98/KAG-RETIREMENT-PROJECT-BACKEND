0705825e1ac72e13e5687871e27cb2a825cb93d5
Yasminopiyo30 <yasminopiyo30@gmail.com>
2026-01-29 22:53:30 +0300

    register church models in admin

--- backend/churches/admin.py
+++ backend/churches/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import Church, ChurchRole, ChurchPastor
 
-# Register your models here.
+@admin.register(Church)
+class ChurchAdmin(admin.ModelAdmin):
+    list_display = ['church_id', 'church_name', 'section', 'location', 'created_at']
+    list_filter = ['section']
+    search_fields = ['church_name', 'location']
+
+@admin.register(ChurchRole)
+class ChurchRoleAdmin(admin.ModelAdmin):
+    list_display = ['role_id', 'role_name', 'created_at']
+    search_fields = ['role_name']
+
+@admin.register(ChurchPastor)
+class ChurchPastorAdmin(admin.ModelAdmin):
+    list_display = ['church', 'pastor', 'role', 'created_at']
+    list_filter = ['role', 'church']
+    search_fields = ['pastor__full_name', 'church__church_name']
