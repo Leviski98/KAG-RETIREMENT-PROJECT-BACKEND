aed4d063ade6011a849e5c1391b1cc03f8050f4f
Leviski Njuguna <leviskinjuguna@gmail.com>
2026-01-30 15:04:34 +0300

    Created a new database

--- backend/districts/serializers.py
+++ backend/districts/serializers.py
@@ -5,5 +5,5 @@ from .models import District
 class DistrictSerializer(serializers.ModelSerializer):
     class Meta:
         model = District
-        fields = ['id', 'name', 'code', 'description', 'created_at', 'updated_at']
+        fields = ['id', 'name', 'created_at', 'updated_at']
         read_only_fields = ['id', 'created_at', 'updated_at']
